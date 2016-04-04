51cf7a3d1caad59af5c90cbe91d5c6f62ff9f4f3
Andrew Howden <hello@andrewhowden.com>
2016-04-04 21:05:27 +1000

    Install hugo, make install-dependencies.sh executable

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -1,10 +1,14 @@
 #!/bin/bin/env bash
 
 GOOGLE_CLOUD_VERSION=103.0.0
+HUGO_VERSION=0.15
 
 # Install Google Cloud
-wget "google-cloud-sdk-$GOOGLE_CLOUD_VERSION-linux-x86_64.tar.gz"
-tar -xvf google-cloud-sdk-$GOOGLE_CLOUD_VERSION-linux-x86_64.tar.gz"
+wget "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GOOGLE_CLOUD_VERSION}-linux-x86_64.tar.gz"
+tar -xvf google-cloud-sdk-${GOOGLE_CLOUD_VERSION}-linux-x86_64.tar.gz"
 ./google-cloud-sdk/bin/gcloud init
 
-
+# Install Hugo
+wget "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
+tar -xvf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
+tree
