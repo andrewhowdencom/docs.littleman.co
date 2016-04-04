19472418d5ebdcae1ece7745b3ab08d16e4f754c
Andrew Howden <hello@andrewhowden.com>
2016-04-04 21:27:51 +1000

    Move Hugo to the appropraite path

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -12,6 +12,4 @@ make auth
 # Install Hugo
 wget "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
 tar -xvf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 /bin/
-
-
+mv hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 /bin/hugo
