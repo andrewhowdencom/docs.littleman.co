183dbc8148b1f2cb3258bf497587bbb6b0233de9
Andrew Howden <hello@andrewhowden.com>
2016-04-04 21:49:35 +1000

    Add a debug statement to the install, add kubernetes install + config

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -3,11 +3,24 @@
 GOOGLE_CLOUD_VERSION=103.0.0
 HUGO_VERSION=0.15
 
+# Defined in Travis
+# GCR_PROJECT
+# GCR_REGION
+# KUBERNETS_CLUSTER
+
 # Install Google Cloud
 wget "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GOOGLE_CLOUD_VERSION}-linux-x86_64.tar.gz"
 tar -xvf google-cloud-sdk-${GOOGLE_CLOUD_VERSION}-linux-x86_64.tar.gz
 export PATH=$PATH:$(pwd)/google-cloud-sdk/bin
+ls -la 
 make auth
+
+# Install kubectl 
+gcloud components update kubectl
+gcloud config set project $GCR_REGION
+gcloud config set compute/zone $GCR_PROJECT
+gcloud config set container/cluster $KUBERNETES_CLUSTER
+gcloud container clusters get-credentials $KUBERNETES_CLUSTER
 
 # Install Hugo
 wget "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
