ebeaa285d7dc1ba40b5522505bffe3e54fcc23f7
Andrew Howden <hello@andrewhowden.com>
2016-04-05 21:18:07 +1000

    Correct the path to gcloud, make the bin dir

--- build/auth.sh
+++ build/auth.sh
@@ -4,13 +4,13 @@
 . /home/travis/.bashrc
 
 # Auth Gcloud
-google-cloud-sdk/gcloud auth activate-service-account ${GCR_SERVICE_ACCOUNT} --key-file .gcloud.json
-google-cloud-sdk/gcloud config set project $GCR_REGION
-google-cloud-sdk/gcloud config set compute/zone $GCR_PROJECT
-google-cloud-sdk/gcloud config set container/cluster $KUBERNETES_CLUSTER
+google-cloud-sdk/bin/gcloud auth activate-service-account ${GCR_SERVICE_ACCOUNT} --key-file .gcloud.json
+google-cloud-sdk/bin/gcloud config set project $GCR_REGION
+google-cloud-sdk/bin/gcloud config set compute/zone $GCR_PROJECT
+google-cloud-sdk/bin/gcloud config set container/cluster $KUBERNETES_CLUSTER
 
 # Auth Kube
-google-cloud-sdk/gcloud container clusters get-credentials $KUBERNETES_CLUSTER
+google-cloud-sdk/bin/gcloud container clusters get-credentials $KUBERNETES_CLUSTER
 
 # Auth Docker
-docker login -e ${GCR_SERVICE_ACCOUNT} -u _token -p "$(google-cloud-sdk/gcloud auth print-access-token)" https://gcr.io
+docker login -e ${GCR_SERVICE_ACCOUNT} -u _token -p "$(google-cloud-sdk/bin/gcloud auth print-access-token)" https://gcr.io
--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -10,6 +10,8 @@ PATH=$PATH:$(pwd)/bin
 # GCR_REGION
 # KUBERNETS_CLUSTER
 
+mkdir bin
+
 # Install Google Cloud
 # Need pyopenssl for gcloud, and need to install it via pip. See https://docs.travis-ci.com/user/languages/python
 # Needs sudo to install pip
@@ -19,19 +21,14 @@ sudo pip install --upgrade pyopenssl ndg-httpsclient pyasn1
 wget "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${GOOGLE_CLOUD_VERSION}-linux-x86_64.tar.gz"
 tar -xvf google-cloud-sdk-${GOOGLE_CLOUD_VERSION}-linux-x86_64.tar.gz
 
-echo "First check of gcloud path: $(type gcloud)"
-
 google-cloud-sdk/install.sh --additional-components bq core gsutil gcloud alpha beta kubectl --quiet
-google-cloud-sdk/kubectl version
+google-cloud-sdk/bin/kubectl version
 
 # Source the installed bashrc
 . /home/travis/.bashrc
 
-echo "Second check of gcloud path: $(type gcloud)"
-
-hash -d gcloud
-gcloud version
-kubectl version
+google-cloud-sdk/bin/gcloud version
+google-cloud-sdk/bin/kubectl version
 
 # Install Hugo
 wget "https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz"
