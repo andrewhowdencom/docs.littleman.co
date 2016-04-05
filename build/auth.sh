0621458edc898eeb1cb960dd2d02e4f8fc60547a
Andrew Howden <hello@andrewhowden.com>
2016-04-05 21:33:45 +1000

    Correct variable usage in auth.sh

--- build/auth.sh
+++ build/auth.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# Terminate on first failure
+set -x
+
+# Set in Travis
+# GCR_REGION
+# GCR_PROJECT
+# KUBERNETS_CLUSTER
+
 # Source .bashrc
 . /home/travis/.bashrc
 
 # Auth Gcloud
 google-cloud-sdk/bin/gcloud auth activate-service-account ${GCR_SERVICE_ACCOUNT} --key-file .gcloud.json
-google-cloud-sdk/bin/gcloud config set project $GCR_REGION
-google-cloud-sdk/bin/gcloud config set compute/zone $GCR_PROJECT
-google-cloud-sdk/bin/gcloud config set container/cluster $KUBERNETES_CLUSTER
+google-cloud-sdk/bin/gcloud config set project ${GCR_PROJECT}
+google-cloud-sdk/bin/gcloud config set compute/zone ${GCR_REGION}
+google-cloud-sdk/bin/gcloud config set container/cluster ${KUBERNETES_CLUSTER}
 
 # Auth Kube
-google-cloud-sdk/bin/gcloud container clusters get-credentials $KUBERNETES_CLUSTER
+google-cloud-sdk/bin/gcloud container clusters get-credentials ${KUBERNETES_CLUSTER}
 
 # Auth Docker
 docker login -e ${GCR_SERVICE_ACCOUNT} -u _token -p "$(google-cloud-sdk/bin/gcloud auth print-access-token)" https://gcr.io
