2eb8dc3268cc42959bb5655be409f35355ecb915
Andrew Howden <hello@andrewhowden.com>
2016-04-05 22:46:21 +1000

    Centralise in the deploy.sh script, update kube path in CI

--- build/scripts/CI/auth.sh
+++ build/scripts/CI/auth.sh
@@ -23,8 +23,8 @@ google-cloud-sdk/bin/gcloud config set container/cluster ${KUBERNETES_CLUSTER}
 google-cloud-sdk/bin/gcloud container clusters get-credentials ${KUBERNETES_CLUSTER}
 
 # Set the context for kube
-export CONTEXT=$(kubectl config view | grep current-context | awk '{print $2}')
-kubectl config set-context $CONTEXT --namespace=${KUBE_NAMESPACE}
+export CONTEXT=$(google-cloud-sdk/bin/kubectl config view | grep current-context | awk '{print $2}')
+google-cloud-sdk/bin/kubectl config set-context $CONTEXT --namespace=${KUBE_NAMESPACE}
 
 # Auth Docker
 docker login -e ${GCR_SERVICE_ACCOUNT} -u _token -p "$(google-cloud-sdk/bin/gcloud auth print-access-token)" https://gcr.io
---
+++ build/scripts/CI/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+make deploy-container-nginx
+
+google-cloud-sdk/bin/kubectl rolling-update webserver --image=gcr.io/${GCR_PROJECT}/nginx:$(git rev-parse --short HEAD)
+
